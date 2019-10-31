33b53e27b810738854a9a20c20976ffe0bd93ec7
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2019-10-31 19:40:35 +0000

    toy2: updating program and spec

--- toy2.v
+++ toy2.v
@@ -9,52 +9,49 @@ From iris.bi Require Import derived_laws_sbi.
 Set Default Proof Using "Type*".
 
 Definition lockNode : val :=
-  rec: "lockN" "x" :=
+  rec: "lockN" "y" :=
     if: CAS "y" #false #true
     then #()
-    else "lockN" "x".
+    else "lockN" "y".
 
 Definition unlockNode : val :=
-  λ: "x",
+  λ: "y",
   "y" <- #false.
 
 Definition prog : val := 
-  λ: "x" "v",
-  lockNode "x";;
+  λ: "x" "y" "v",
+  lockNode "y";;
   "x" <- "v";;
-  unlockNode "x";; "v".
+  unlockNode "y";; "v".
 
 Section Toy_Template.
   Context `{!heapG Σ} (N : namespace).
   Notation iProp := (iProp Σ).
 
-(*
-  Definition toyN : namespace := N .@ "toy".
+  (* We should be able to prove the following specs: *)
+  
+  Lemma lock_spec (y: loc) :
+    <<< ∀ (b: bool), y ↦ #b >>>
+        lockNode #y @ ⊤
+    <<< y ↦ #true ∗ if b then False else True, RET #() >>>.
+  Proof. Admitted.
 
-  Definition main_inv x y (b: bool) d := (y ↦ #b ∗ if b then True else x ↦ d)%I.
+  Lemma unlock_spec (y: loc) :
+    <<< y ↦ #true >>>
+        unlockNode #y @ ⊤
+    <<< y ↦ #false, RET #() >>>.
+  Proof. Admitted.
 
-  Definition is_toy x y := inv toyN (∃ b d, main_inv x y b d)%I.
+  Definition is_locked_ref x y v : iProp :=
+    (∃ (b: bool), y ↦ #b ∗ if b then True else x ↦ v)%I.
 
-  Instance main_inv_timeless x y b d: Timeless (main_inv x y b d).
+  Lemma prog_spec (x y: loc) (v: val) :
+    <<< ∀ (u: val), is_locked_ref x y u >>>
+        prog #x v @ ⊤
+    <<< is_locked_ref x y v, RET #() >>>.
   Proof.
-    rewrite /main_inv. repeat apply bi.sep_timeless; try apply _.
-    destruct b; try apply _.
-  Qed.
-
-  Lemma lockNode_spec (x y: loc):
-      is_toy x y -∗
-      <<< True >>> lockNode #x @ ⊤∖↑toyN <<< y ↦ #true, RET #() >>>.
-  Proof.
-    iIntros "#HInv" (Φ) "AU". iLöb as "IH".
-    wp_lam. wp_bind (CmpXchg _ _ _)%E.
-    iInv "HInv" as "HI".
-*)
-
-  Definition main_inv x y := (∃ (b:bool) d, y ↦ #b ∗ if b then True else x ↦ d)%I.
-
-  Lemma lockNode_spec (x y: loc):
-      <<< main_inv x y >>> lockNode #x @ ⊤ <<< main_inv x y ∗ y ↦ #true, RET #() >>>.
-  Proof.
-    iIntros (Φ) "AU". iLöb as "IH".
+    unfold is_locked_ref.
+    iIntros (Φ) "HP". iLöb as "IH".
     wp_lam. wp_bind (CmpXchg _ _ _)%E.
     iMod "AU" as "[hoho [_ HClose]]".
+
