1fc8c88c7d27602b752e6738b6c469b0d087efc0
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2019-11-07 12:48:22 -0500

    recursive_toy: more realistic specs

--- recursive_toy.v
+++ recursive_toy.v
@@ -7,39 +7,40 @@ Set Default Proof Using "Type*".
 Variable findNext : val.
 
 Definition traverse : val :=
-  rec: "tr" "n" "v" :=
-    match: (findNext "n" "v") with
+  rec: "tr" "n" :=
+    match: (findNext "n") with
       NONE => "n"
-    | SOME "n'" => "tr" "n'" "v"
+    | SOME "n'" => "tr" "n'"
     end.
 
 Section stack_model.
-
   Context `{!heapG Σ}.
   Notation iProp := (iProp Σ).
 
-  Parameter findNext_spec : ∀ (x: loc) (X: gset loc) v,
-    (<<< ∀ w, ⌜x ∈ X⌝ ∗ x ↦ #w >>>
-         findNext #x #v @ ⊤
-     <<< ∃(b:bool) (y:loc), x ↦ #w ∗ match b with true => ⌜#v = #w⌝ |
-                                        false => ∃ c, y ↦ c ∗ ⌜y ∈ X⌝ end,
-                           RET (match b with true => NONEV |
-                                      false => SOMEV #y end) >>>)%I.
+  Lemma findNext_spec (X: gset loc) (x: loc):
+    <<< ([∗ set] n ∈ X, ∃ v: val, n ↦ v) ∗ ⌜x ∈ X⌝ >>>
+      findNext #x @ ⊤
+    <<< ∃ (b: bool) (y: loc), ([∗ set] n ∈ X, ∃ v: val, n ↦ v)
+        ∗ ⌜b → y ∈ X⌝,
+        RET (match b with true => SOMEV #y | false => NONEV end)
+    >>>.
+  Proof. Admitted. (* Omitting this proof for clarity *)
 
-
-  Lemma traverse_spec (x: loc) v (X: gset loc):
-    <<< ∀ w, ⌜x ∈ X⌝ ∗ x ↦ #w >>>
-              traverse #x #v
-                    @ ⊤
-         <<< ∃ (y: loc), y ↦ #v ∗ ⌜y ∈ X⌝, RET #y >>>.
+  Lemma traverse_spec (X: gset loc) (x: loc):
+    <<< ([∗ set] n ∈ X, ∃ v: val, n ↦ v) ∗ ⌜x ∈ X⌝ >>>
+      traverse #x @ ⊤
+    <<< ∃ (y: loc), ([∗ set] n ∈ X, ∃ v: val, n ↦ v) ∗ ⌜y ∈ X⌝, RET #y >>>.
   Proof.
     iLöb as "IH" forall (x). iIntros (Φ) "AU".
-    wp_lam. wp_let. wp_bind (findNext _ _)%E. awp_apply (findNext_spec x X v).
-    iApply (aacc_aupd_abort with "AU"); first done. iIntros (w) "(#Hin & Hx)".
+    wp_lam. wp_bind (findNext _ )%E. awp_apply (findNext_spec X x).
+    iApply (aacc_aupd_abort with "AU"); first done. iIntros "(HNs & #Hin)".
+    (* Sid: can you update the proof from here on? *)
     iAssert (⌜x ∈ X⌝ ∗ x ↦ #w)%I with "[Hx]" as "Haac". eauto with iFrame.
     iAaccIntro with "Haac". eauto with iFrame. 
     iIntros (b y) "(Hx & Hb)". destruct b.
     - admit.
+      (* Sid: we know how to prove the b == true case right? If so, add a 
+        comment saying so. *)
     - iModIntro. iSplitL "Hx". eauto with iFrame. iIntros "AU".
       iModIntro. wp_match. iApply "IH".
   Admitted.
