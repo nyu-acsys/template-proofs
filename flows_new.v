a71dc9aa8f5c9a164b8acf0bf9e42dbd747f0501
wies <wies@cs.nyu.edu>
2020-01-04 11:36:55 -0500

    fix definitions

--- flows_new.v
+++ flows_new.v
@@ -73,6 +73,23 @@ Instance flowint_valid : Valid flowintT :=
        | intUndef => False
        end.
 
+Instance int_eq_dec: EqDecision flowintT.
+Proof.
+  unfold EqDecision.
+  unfold Decision.
+  repeat decide equality.
+  all: apply gmap_eq_eq.
+Qed.
+
+Instance flowint_valid_dec (I: flowintT) : Decision (flowint_valid I).
+Proof.
+  intros.
+  unfold Decision.
+  unfold flowint_valid.
+  destruct I.
+  admit.
+Admitted.
+
 Instance intComposable_dec (I1 I2: flowintT) : ∀ (i: Node) (x: M), Decision ((λ (n : Node) _, inf I1 n = out I2 n + (inf I1 n - out I2 n)) i x).
 Proof.
   intros.
@@ -81,6 +98,7 @@ Proof.
 Qed.
 
 Definition intComposable (I1: flowintT) (I2: flowintT) :=
+  flowint_valid I1 ∧ flowint_valid I2 ∧
   dom I1 ## dom I2 ∧
   map_Forall (λ (n: Node) (m: M), inf I1 n = out I2 n + (inf I1 n - out I2 n)) (inf_map I1) ∧
   map_Forall (λ (n: Node) (m: M), inf I2 n = out I1 n + (inf I2 n - out I1 n)) (inf_map I2).
@@ -107,5 +125,7 @@ Instance intComp : Op flowintT :=
              in
              let out12 := gmap_imerge M M M f_out (out_map I1) (out_map I2) in
              int {| infR := inf12; outR := out12 |}
-           else intUndef.
+           else if decide (I1 = I_empty) then I2
+                else if decide (I2 = I_empty) then I1
+                     else intUndef.
 
