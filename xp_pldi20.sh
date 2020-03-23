943689d2b4f6dbb27c81fe6adceb2f69856d070c
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-03-23 12:49:18 -0400

    xp_pldi20: fix typo

--- xp_pldi20.sh
+++ xp_pldi20.sh
@@ -25,7 +25,7 @@ run()
         { TIMEFORMAT=%3R; time make $f.vo 2>&1 ; } 2>> $timesfile
         retcode=$?
         if [ $retcode -ne 0 ]; then
-            echo -e "\nCoq exited with errorso n file $f.v.\n"
+            echo -e "\nCoq exited with errors in file $f.v.\n"
         fi
         echo 1 >> $timesfile
     done
