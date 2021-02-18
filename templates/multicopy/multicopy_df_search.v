97d0bfd4f90b62c18e0b823d115ebf25e1eda7fc
eadietri <eadietri@iu.edu>
2021-02-18 11:49:14 -0500

    Correct df search file with changes to cases

--- templates/multicopy/multicopy_df_search.v
+++ templates/multicopy/multicopy_df_search.v
@@ -13,29 +13,29 @@ Section multicopy_df_search.
   Context {Σ} `{!heapG Σ, !multicopyG Σ, !multicopy_dfG Σ}.
   Notation iProp := (iProp Σ).
   Local Notation "m !1 i" := (nzmap_total_lookup i m) (at level 20).
-
-  Lemma search_recency N γ_te γ_he γ_s Prot
+  
+  Lemma search_recency N γ_te γ_he γ_s Prot 
                        γ_t γ_cr γ_cd γ_d lc r d (k: K) t0 :
-    ⊢ ⌜k ∈ KS⌝ -∗
+    ⊢ ⌜k ∈ KS⌝ -∗ 
         mcs_inv N γ_te γ_he γ_s Prot
            (Inv_DF γ_s γ_t γ_cr γ_cd γ_d lc r d) -∗
           SR γ_s (k, t0) -∗
-              <<< True >>>
+              <<< True >>> 
                   search r d #k @ ⊤ ∖ ↑(mcsN N)
               <<< ∃ (t': nat), SR γ_s (k, t') ∗ ⌜t0 ≤ t'⌝ , RET #t' >>>.
   Proof.
     iIntros "% #HInv #mcs_sr" (Φ) "AU".
-    rename H into k_in_KS.
-    iApply fupd_wp.
+    rename H into k_in_KS. 
+    iApply fupd_wp. 
     iInv "HInv" as (T H)"(mcs_high & >Inv_DF)".
-    iDestruct "Inv_DF" as (Cr Cd)"(Ho & Hstar)".
-    iDestruct "mcs_high" as "(>MCS_auth & >HH & >#Hist & >#MaxTS & Prot)".
-    iDestruct "Hstar" as "(#Hrnotd & #Hcir & Hset & Hlockbr
+    iDestruct "Inv_DF" as (Cr' Cd')"(Ho & Hstar)".
+    iDestruct "mcs_high" as "(>MCS_auth & >HH & >Hist & >MaxTS & Prot)".
+    iDestruct "Hstar" as "(Hrnotd & #Hcir & Hset & Hlockbr 
             & Hycr & Hlockbd & Hycd)".
     rewrite (big_sepS_delete _ (KS) k); last by eauto.
     iDestruct "Hset" as "(HnS_stark & HnS_star')".
-    iMod (own_update (γ_d !!! k) (● MaxNat (Cd !!! k))
-          (● (MaxNat (Cd !!! k)) ⋅ ◯ (MaxNat (Cd !!! k)))
+    iMod (own_update (γ_d !!! k) (● MaxNat (Cd' !!! k)) 
+          (● (MaxNat (Cd' !!! k)) ⋅ ◯ (MaxNat (Cd' !!! k))) 
             with "[$HnS_stark]") as "HnS_stark".
     { apply (auth_update_frac_alloc); try done.
       unfold CoreId, pcore, cmra_pcore. simpl.
@@ -51,14 +51,16 @@ Section multicopy_df_search.
 
     iModIntro. iSplitR "AU". iNext.
     iExists T, H. iFrame. iFrame "#".
-    iExists Cr, Cd. iFrame. iFrame "#".
+    iExists Cr', Cd'. iFrame. iFrame "#".
     rewrite (big_sepS_delete _ (KS) k); last by eauto.
-    iFrame "#∗".
+    iFrame "#∗". iModIntro. wp_lam.
 
-    iModIntro. wp_lam. awp_apply lockNode_spec_high; try done.
-    iPureIntro. left. done.
-    iAaccIntro with ""; try eauto with iFrame.
-    iIntros (γ_cn Cn T1)"HnP_n". iModIntro. wp_pures.
+      (* Might need something here *)
+
+    awp_apply lockNode_spec_high; try done.
+    iPureIntro. left. done. 
+    iAaccIntro with ""; try eauto with iFrame. 
+    iIntros (γ_cn Cn T1)"HnP_n". iModIntro. wp_pures. 
     iDestruct "HnP_n" as "(HnP_n & #HRorD)".
     iDestruct "HnP_n" as "(Hnode & #Hγ_s & Hγ_c & Hdecide)".
     wp_apply (inContents_spec with "Hnode").
@@ -76,130 +78,80 @@ Section multicopy_df_search.
       iAaccIntro with ""; try eauto with iFrame.
       iIntros "_". iModIntro. wp_pures.
 
-    (** Unlock node n **)
-    awp_apply (unlockNode_spec_high with "[] []
-    [Hγ_c Hdecide node_n]"); try done. iFrame "∗#".
-    iAaccIntro with ""; try eauto with iFrame.
-    iIntros "_". iModIntro. wp_pures.
+      awp_apply lockNode_spec_high; try done. iPureIntro.
+      right. done. iAaccIntro with ""; try eauto with iFrame.
+      iIntros (γ_cn' Cr T2)"HnP_n". iModIntro. wp_pures. 
+      iDestruct "HnP_n" as "(HnP_n & #HRorD')".
+      iDestruct "HnP_n" as "(Hnode' & #Hγ_s' & Hγ_c' & Hdecide')".
 
-    awp_apply lockNode_spec_high; try done. iPureIntro.
-    right. done. iAaccIntro with ""; try eauto with iFrame.
-    iIntros (γ_cn' Cr' T2)"HnP_n". iModIntro. wp_pures.
-    iDestruct "HnP_n" as "(HnP_n & #HRorD')".
-    iDestruct "HnP_n" as "(Hnode' & #Hγ_s' & Hγ_c' & Hdecide')".
-
-    wp_apply (inContents_spec with "Hnode'").
-    iIntros (t) "(node_n & H)". iDestruct "H" as %Cn_val'.
-    wp_pures.
-    (** Case analysis on whether k in contents of n **)
-    destruct t as [t |]; last first.
-    +  (* k is not in the data structure. *)
+      wp_apply (inContents_spec with "Hnode'").
+      iIntros (t) "(node_n & H)". iDestruct "H" as %Cn_val'.
       wp_pures.
-      (** Unlock node n **)
-      awp_apply (unlockNode_spec_high with "[] []
-      [Hγ_c' Hdecide' node_n]"); try done. iFrame "∗#".
-      iAaccIntro with ""; try eauto with iFrame.
-      iIntros "_". iModIntro. wp_pures.
+      destruct t as [t |]; last first. wp_pures.
 
-      iMod "AU" as "[_ [_ Hclose]]".
-      iSpecialize ("Hclose" $! t0).
-      iMod ("Hclose" with "[]") as "HΦ".
-      iSplitR; try done.
-      iModIntro.
-      (* not sure if we need this assert *)
-      iAssert (⌜(k,0) ∈ H⌝)%I as "%".
+      (** Unlock node n **)       
+      awp_apply (unlockNode_spec_high with "[] [] 
+      [Hγ_c' Hdecide' node_n]"); try done. iFrame "∗#". 
+      iAaccIntro with ""; try eauto with iFrame.
+      iIntros "_". 
+
+    iAssert (⌜t0 = 0⌝)%I as %t0_zero. 
+      { iPureIntro. admit. } subst t0.
+    iMod "AU" as "[_ [_ Hclose]]". 
+    iInv "HInv" as (T' H')"(mcs_high & >Inv_DF)". {admit. }
+    iDestruct "mcs_high" as "(>MCS_auth & >HH & >Hist & >MaxTS & Prot)".
+    iAssert (⌜(k,0) ∈ H'⌝)%I as "%".
       { iDestruct "Hist" as %Hist. iPureIntro.
         by pose proof Hist k k_in_KS as Hist. }
+    iSpecialize ("Hclose" $! 0).
+    iMod (own_update γ_s (● H') (● H' ⋅ ◯ {[(k,0)]}) with "[$HH]") as "HH".
+        { apply (auth_update_frac_alloc _ H' ({[(k,0)]})).
+          apply gset_included. set_solver. }
+    iDestruct "HH" as "(HH & #mcs_sr'')".
+    iModIntro. 
+    iSplitR "HInv Hclose". iNext. 
+    iExists T', H'. iFrame. 
+    iMod ("Hclose" with "[]") as "HΦ". iFrame "mcs_sr''". by iPureIntro. 
+    (** Closing the invariant **)
+    iModIntro. wp_pures. iFrame. wp_pures.
+    awp_apply (unlockNode_spec_high with "[] [] [Hγ_c' Hdecide' node_n]"); try  done.
+    iFrame. iFrame "#".
+    iAaccIntro with ""; try done.
+    { eauto with iFrame. } iIntros "_".
+    iMod "AU" as "[_ [_ Hclose]]".
+    iInv "HInv" as (T' H')"(mcs_high & >Inv_DF)". {admit. }
+    iDestruct "mcs_high" as "(>MCS_auth & >HH & >Hist & >MaxTS & Prot)".
+    iAssert (⌜(k,0) ∈ H'⌝)%I as "%".
+    { iDestruct "Hist" as %Hist. iPureIntro.
+      by pose proof Hist k k_in_KS as Hist. }
+    iAssert (⌜t0 ≤ t⌝)%I as %t0_lt_t.
+      {admit. } 
+    iSpecialize ("Hclose" $! t0).
+    iMod (own_update γ_s (● H') (● H' ⋅ ◯ {[(k,0)]}) with "[$HH]") as "HH".
+        { apply (auth_update_frac_alloc _ H' ({[(k,0)]})).
+          apply gset_included. set_solver. }
+    iDestruct "HH" as "(HH & mcs_sr'')". iModIntro.
+    iSplitR "HInv Hclose". iNext. 
+    iExists T', H'. iFrame. 
+    iMod ("Hclose" with "[]") as "HΦ".
+    iFrame "mcs_sr". by iPureIntro. 
+    iModIntro. wp_pures. 
+    { admit. }
 
-      admit.
-    + (* k is in node d *)
-      awp_apply (unlockNode_spec_high with "[] [] [Hγ_c' Hdecide' node_n]"); try done.
-      iFrame. iFrame "#".
-      iAaccIntro with ""; try done.
-      { eauto with iFrame. }
-
-      iMod "AU" as "[_ [_ Hclose]]".
-      iSpecialize ("Hclose" $! t).
-      iMod ("Hclose" with "[]") as "HΦ".
-      iAssert (⌜t0 ≤ t⌝)%I as %t0_lt_t.
-      {
-        admit.
-      }
-      iSplitL. unfold SR.
-      {
-        admit.
-      }
-      iPureIntro; done.
-
-      iIntros. iModIntro. wp_pures.
-      iModIntro. done.
-  - (* Case 2: k is in the root node. *)
+  - (* Case 2: k is in the root node. *)  
+      wp_pures.
       awp_apply (unlockNode_spec_high with "[] [] [Hγ_c Hdecide node_n]"); try done.
       iFrame. iFrame "#".
       iAaccIntro with ""; try done.
       { eauto with iFrame. }
-
       iMod "AU" as "[_ [_ Hclose]]".
-      iSpecialize ("Hclose" $! t).
+      iSpecialize ("Hclose" $! t0).
       iMod ("Hclose" with "[]") as "HΦ".
       iAssert (⌜t0 ≤ t⌝)%I as %t0_lt_t.
-      {
-        admit.
-      }
-      iSplitL. unfold SR.
-      {
-        admit.
-      }
-      iPureIntro; done.
-
-      iIntros. iModIntro. wp_pures.
-      iModIntro. done.
+      {admit. } 
+      iFrame "mcs_sr". by iPureIntro.
+      iIntros "_". 
+      iModIntro. wp_pures. 
+      {admit. }
+  
   Qed.
- (*   iAssert (⌜Cr' !!! k = Cd !!! k⌝)%I as %Cn_eq_Cd.
-    { iDestruct "Hcir" as %Hcir.
-    pose proof Hcir k t0 as [_ H'].
-    iPureIntro. pose proof H' Cn_val' as H'.
-    rewrite /(Bn !!! k). unfold finmap_lookup_total.
-    by rewrite H'.  }
-
-      { iDestruct "Hcir" as %Hcir.
-        pose proof Hcir k t0 as [_ H'].
-        iPureIntro.
-        rewrite !lookup_total_alt.
-        pose proof H' Cn_val as H'.
-        by rewrite Cn_val H'. }
-
-    iAssert (⌜Cd !!! k = 0⌝)%I as %Cd_eq_0.
-    { iDestruct "Hcir" as %Hcir.
-      pose proof Hcir k as [_ H'].
-      pose proof H' Cn_val' as H'.
-      iPureIntro.
-      rewrite lookup_total_alt.
-      rewrite H' Hφ1. by simpl. }
-    iEval (rewrite (big_sepS_elem_of_acc (_) (KS) k); last by eauto)
-                                                   in "HnS_star".
-    iDestruct "HnS_star" as "(Hcirk_n & HnS_star')".
-    iAssert (⌜t1 ≤ Bn !!! k⌝)%I as %lb_t1.
-    { iPoseProof (own_valid_2 with "[$Hcirk_n] [$Hlb]") as "%".
-      rename H1 into Valid_Bnt.
-      apply auth_both_valid_discrete in Valid_Bnt.
-      destruct Valid_Bnt as [H' _].
-      apply max_nat_included in H'.
-      simpl in H'. by iPureIntro. }
-    iAssert (⌜t0 = 0⌝)%I as %t0_zero.
-    { iPureIntro. rewrite Bn_eq_0 in lb_t1.
-      clear -lb_t1 t0_le_t1. lia. } subst t0.
-    (** Linearization **)
-    iMod "AU" as "[_ [_ Hclose]]".
-    iDestruct "mcs_high" as "(>MCS_auth & >HH & >Hist & >MaxTS & Prot)".
-    iAssert (⌜(k,0) ∈ H⌝)%I as "%".
-    { iDestruct "Hist" as %Hist. iPureIntro.
-      by pose proof Hist k k_in_KS as Hist. }
-    rename H1 into k0_in_H.
-    iSpecialize ("Hclose" $! 0).
-    iMod (own_update γ_s (● H) (● H ⋅ ◯ {[(k,0)]}) with "[$HH]") as "HH".
-    { apply (auth_update_frac_alloc _ H ({[(k,0)]})).
-      apply gset_included. clear -k0_in_H. set_solver. }
-    iDestruct "HH" as "(HH & #mcs_sr)".
-    iMod ("Hclose" with "[]") as "HΦ". iFrame "mcs_sr".
-    by iPureIntro.*)
